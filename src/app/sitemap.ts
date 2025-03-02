97b286a631bb1f20e53041bcf27d4da1628cfa2f
ArmanGrigorian <armangrigorian.work@gmail.com>
2025-03-02 15:35:51 +0400

    update: chnaged the base url

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,25 +3,25 @@ import type { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://emorya.vercel.app",
+      url: "https://emorya-six.vercel.app/",
       lastModified: new Date(),
       changeFrequency: "yearly",
       priority: 1,
     },
     {
-      url: "https://emorya.vercel.app/becomeAmbassador",
+      url: "https://emorya-six.vercel.app/becomeAmbassador",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.7,
     },
     {
-      url: "https://emorya.vercel.app/team",
+      url: "https://emorya-six.vercel.app/team",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.7,
     },
     {
-      url: "https://emorya.vercel.app/tokenomics",
+      url: "https://emorya-six.vercel.app/tokenomics",
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 0.8,
